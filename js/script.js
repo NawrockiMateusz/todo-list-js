9c18c273f954b72e4d8510d2a8dafa92183a18e5
Mateusz Nawrocki <mateusz.nawroo@gmail.com>
2021-10-31 10:47:44 +0100

    binding evetns moved to a seperate function

--- js/script.js
+++ js/script.js
@@ -19,6 +19,24 @@
         render();
     }
 
+    const bindEvenets = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    }
+
 
     const render = () => {
         let htmlString = "";
@@ -37,21 +55,7 @@
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
-        const removeButtons = document.querySelectorAll(".js-remove");
-
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-                removeTask(index);
-            });
-        });
-
-        const toggleDoneButtons = document.querySelectorAll(".js-done");
-
-        toggleDoneButtons.forEach((toggleDoneButton, index) => {
-            toggleDoneButton.addEventListener("click", () =>{
-                toggleTaskDone(index);
-            });
-        });
+        bindEvenets();
     };
 
 
